d180837b94345500c053b97ed94c06efe84c7d45
SEBASTIAN HERNANDEZ VIVANCO <seba.hernandezv@duocuc.cl>
2025-08-18 09:01:30 -0400

    modified contact.js

--- api/test.js
+++ api/test.js
@@ -1,18 +1,18 @@
-// temporal
+// // temporal
 
-export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS, GET");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+// export default function handler(req, res) {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS, GET");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
+//   if (req.method === "OPTIONS") {
+//     res.status(200).end();
+//     return;
+//   }
 
-  res.status(200).json({
-    method: req.method,
-    message: "Test endpoint funcionando",
-    timestamp: new Date().toISOString(),
-  });
-}
+//   res.status(200).json({
+//     method: req.method,
+//     message: "Test endpoint funcionando",
+//     timestamp: new Date().toISOString(),
+//   });
+// }
